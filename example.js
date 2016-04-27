ca7d3e8a8d6bc6c9b80ac1aa032443d5111c21f0
kumavis <aaron@kumavis.me>
2016-04-26 21:13:31 -0700

    we need to go deeper

--- example.js
+++ example.js
@@ -3,16 +3,20 @@ const EthStore = require('./index')
 const ZeroClient = require('web3-provider-engine/zero')
 
 var engine = ZeroClient({
-  // rpcUrl: 'https://rawtestrpc.metamask.io/',
-  rpcUrl: 'https://rawrpc.metamask.io/',
+  rpcUrl: 'https://testrpc.metamask.io/',
 })
 
 // var query = new EthQuery(engine)
-var store = new EthStore(engine)
+var store = new EthStore(engine, engine)
+
+// store.addAccount('0x18a3462427bcc9133bb46e88bcbe39cd7ef0e761')
+// store.addTransaction('0x0eebfb085d67ef47a1acdd04e8af0a181e48eaf23843dd6125960080818ff2e0')
+
+store.put('myBalance', {
+  method: 'eth_getBalance',
+  params: ['0x18a3462427bcc9133bb46e88bcbe39cd7ef0e761'],
+})
 
 store.on('block', function(state){
   console.log(state)
 })
-
-store.addAccount('0x18a3462427bcc9133bb46e88bcbe39cd7ef0e761')
-store.addTransaction('0x0eebfb085d67ef47a1acdd04e8af0a181e48eaf23843dd6125960080818ff2e0')
