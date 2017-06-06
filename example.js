725e4040a9cdb6fcb3944d0fd3c00c8b563979b1
kumavis <aaron@kumavis.me>
2017-06-06 14:06:46 -0700

    3.0.0

--- example.js
+++ example.js
@@ -1,16 +1,10 @@
 const EthStore = require('./index')
-// const EthQuery = require('./query')
 const ZeroClient = require('web3-provider-engine/zero')
 
-var engine = ZeroClient({
-  rpcUrl: 'https://testrpc.metamask.io/',
-})
+const provider = web3.currentProvider
+const blockTracker = new EthBlockTracker({ provider })
 
-// var query = new EthQuery(engine)
-var store = new EthStore(engine, engine)
-
-// store.addAccount('0x18a3462427bcc9133bb46e88bcbe39cd7ef0e761')
-// store.addTransaction('0x0eebfb085d67ef47a1acdd04e8af0a181e48eaf23843dd6125960080818ff2e0')
+const store = new EthStore(blockTracker, provider)
 
 store.put('myBalance', {
   method: 'eth_getBalance',
@@ -18,5 +12,6 @@ store.put('myBalance', {
 })
 
 store.on('block', function(state){
+  // { myBalance: '0xabcd' }
   console.log(state)
 })
